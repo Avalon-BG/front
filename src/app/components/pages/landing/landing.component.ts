ff779aa42681d7fd67fcd37fde2d73b3a3f7889a
Romain <romain.thierry@safety-line.fr>
2022-05-27 17:16:55 +0200

    feat(v2): WIP - landing app

--- src/app/components/pages/landing/landing.component.ts
+++ src/app/components/pages/landing/landing.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
 import { Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
+
+const cardWidth = 232;
 
 @Component({
   selector: 'app-landing',
@@ -8,28 +10,47 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrls: ['./landing.component.scss'],
   host: { class: 'component-container' }
 })
-export class LandingComponent implements OnInit {
+export class LandingComponent implements OnInit, OnDestroy {
 
   usedLang = 'en';
-  nbFireflies: number;
+  cardDisplayed = 0;
+  transform = "translateX(0px)";
+  intervalId: number | undefined;
 
   constructor(private router: Router, private translate: TranslateService) {
     this.usedLang = this.translate.currentLang;
     this.translate.onLangChange.subscribe(changes => {
       this.usedLang = changes.lang;
     });
-    this.nbFireflies = 15;
+  }
+  ngOnDestroy(): void {
+    clearInterval(this.intervalId);
   }
 
   ngOnInit() {
+    this.setInterval()
   }
 
-  get fireflies(): Array<number> {
-    return Array.from(Array(this.nbFireflies).keys());
+  setInterval() {
+    this.intervalId = window.setInterval(() => {
+      this.cardDisplayed = (this.cardDisplayed + 1) % 3;
+      this.setTransform(this.cardDisplayed)
+    }, 5000);
   }
 
   launchGame() {
     this.router.navigate(['dashboard']);
+  }
+
+  setTransform(cardNumber: number) {
+    this.transform = `translateX(-${cardWidth * cardNumber}px)`;
+  }
+
+  select(cardNumber: number) {
+    this.cardDisplayed = cardNumber;
+    this.setTransform(this.cardDisplayed);
+    clearInterval(this.intervalId);
+    this.setInterval();
   }
 
   get pdfUrl(): string {
